4a529e2f6d8d3d2cd9c1d2c12b3cf00685e05b45
Tomasz Ostaszewski <ost.tomasz@gmail.com>
2024-11-02 17:54:20 +0100

    task 3 complete

--- goit-react-hw-01/src/App.jsx
+++ goit-react-hw-01/src/App.jsx
@@ -4,11 +4,15 @@ import userData from "./userData.json";
 import { FriendList } from "./components/FriendList/FriendList";
 import friends from "./friends.json";
 
+import { TransactionHistory } from "./components/TransactionHistory/TransactionHistory";
+import transactions from "./transactions.json";
+
 export const App = () => {
   return (
     <main>
       <Profile profile={userData} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </main>
   );
 };
