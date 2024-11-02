c121cae15d1f0b55d8fbf078af54b363d49f5ad5
Tomasz Ostaszewski <ost.tomasz@gmail.com>
2024-11-02 17:21:57 +0100

    task 2 style complete

--- goit-react-hw-01/src/components/FriendList/Friendlist.jsx
+++ goit-react-hw-01/src/components/FriendList/Friendlist.jsx
@@ -1,14 +1,23 @@
-// import css from "./FriendList.module.css";
+import clsx from "clsx";
+
+import css from "./FriendList.module.css";
 
 export const FriendList = ({ friends }) => {
   return (
-    <ul>
+    <ul className={css["friend-list"]}>
       {friends.map((friend) => {
         return (
-          <li key={friend.id}>
-            <img src={friend.avatar} alt={friend.name} width="48" />
-            <p>{friend.name}</p>
-            <p>{friend.isOnline ? "Online" : "Offline"}</p>
+          <li className={css.friend} key={friend.id}>
+            <img src={friend.avatar} alt={friend.name} width="100px" />
+            <p className={css.name}>{friend.name}</p>
+            <p
+              className={clsx(
+                css.status,
+                friend.isOnline ? css.online : css.offline
+              )}
+            >
+              {friend.isOnline ? "Online" : "Offline"}
+            </p>
           </li>
         );
       })}
