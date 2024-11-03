e27b9af8e3d1bef98bbb1c62210e42b648c64712
Tomasz Ostaszewski <ost.tomasz@gmail.com>
2024-11-03 10:25:28 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { Profile } from "./components/Profile/Profile";
+import { Profile } from "./components/Profile/Profile.jsx";
 import userData from "./userData.json";
 
-import { FriendList } from "./components/FriendList/FriendList";
+import { FriendList } from "./components/FriendList/FriendList.jsx";
 import friends from "./friends.json";
 
-import { TransactionHistory } from "./components/TransactionHistory/TransactionHistory";
+import { TransactionHistory } from "./components/TransactionHistory/TransactionHistory.jsx";
 import transactions from "./transactions.json";
 
 export const App = () => {
